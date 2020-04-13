c5bb3e1492dd1e012ebc20f66ed0988909cb1bba
Hugh Wang <18501667737@163.com>
2020-04-13 12:27:26 +0800

    by bash

---
+++ app/src/main/java/com/example/k12_math_demo/K12Application.java
@@ -0,0 +1,28 @@
+package com.example.k12_math_demo;
+
+import android.app.Application;
+import android.content.Context;
+import org.scilab.forge.jlatexmath.core.AjLatexMath;
+
+import io.github.kbiakov.codeview.classifier.CodeProcessor;
+
+/**
+ * 自定义应用-Application
+ */
+public class K12Application extends Application {
+
+    public static Context appContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appContext = this;
+
+
+        //init flexiblerichtextview
+        AjLatexMath.init(this);
+        //code highlight
+        CodeProcessor.init(this);
+    }
+
+}
--- app/src/main/java/com/example/k12_math_demo/MainActivity.java
+++ app/src/main/java/com/example/k12_math_demo/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.k12_math_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.daquexian.flexiblerichtextview.FlexibleRichTextView;
+import com.daquexian.flexiblerichtextview.Tokenizer;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlexibleRichTextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.id_rich_tv);
+        show();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public  void show(){
+        Tokenizer.setImageLabels("<img height=\\h width=\\w>\\u</img>");
+        Tokenizer.setColorStartLabel("<span style=\"color:\\s\">");
+        Tokenizer.setColorEndLabels("</span>");
+        Tokenizer.setBoldStartLabels("<strong>", "[strong]");
+        Tokenizer.setBoldEndLabels("</strong>", "[/strong]");
+
+        textView.setText("<img height=200 width=100>https://wx4.sinaimg.cn/mw690/6204ece1ly1gdrycnc8ppj204x06wt8k.jpg</img> 比较大小：\\[{{x}^{2}}\\]+4x-3          6x-4  <strong><span style=\"color:#f32784\">hello</span></strong></p >");
+//        flexibleRichTextView.setText("已知集合\\(A=\\{-1,1\\}\\)，\\(B=\\{x|mx=1\\}\\)，且\\(A\\cup B=A\\)，则\\(m\\)的值为________");
+
     }
 }
